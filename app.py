38bb2f2d667ac27fefe54aaf81403a161f167dc2
DarylNg514 <ngueufangdaryl99@gmail.com>
2024-06-04 17:42:06 -0400

    last push
    last push

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import snowflake.connector as sc
+import pandas as pd
+
+con = sc.connect(
+user="Daryl",
+password="Daryl514",
+account="hyoulpz-bc97192"
+)
+cursor= con.cursor()
+
 def main():
-    st.write("# Welcome to Snowflake demo")
+    st.write("# Welcome to Snowflake demo") 
+    res=con.cursor().execute("select * from RCW.PERSONNE.PERSONNNES ;").fetchall()
+    st.write(f'Value of table in snowflake is : {res}')
+    # ou 
+    st.write(personnes())
     
+def personnes():
+    sql="select * from RCW.PERSONNE.PERSONNNES ;"
+    df= cursor.execute(sql).fetchall()
+    return pd.DataFrame(df) 
 if __name__ == '__main__':
     main()
---
+++ pages/affiche_column.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import snowflake.connector as sc
+import pandas as pd
+
+def main():
+    try:
+        con = sc.connect(
+        user="Daryl",
+        password="Daryl514",
+        account="hyoulpz-bc97192"
+        )
+        if con is None:
+            st.warning("Vos information de connection a snowflake sont incorrect")
+
+        cursor= con.cursor()
+        def datapersonnes():
+            sql="select * from RCW.PERSONNE.PERSONNNES ;"
+            cursor.execute(sql)
+            colonne_names=[]
+            for column in cursor.description:
+                colonne_names.append(column[0])
+            data= cursor.fetchall()
+            #column_names= [desc[0] for desc in cursor.description]
+            df= pd.DataFrame(data=data,columns=colonne_names) 
+            return df
+        st.write("# Affichage le nom des colonne") 
+        st.write(datapersonnes())
+    except:
+        st.warning("Vos information de connection a snowflake sont incorrect")
+
+if __name__ == '__main__':
+    main()
